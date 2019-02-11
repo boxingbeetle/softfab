7e91aa898668f5a057336ee9aa3960020a52dbf3
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-02-11 15:03:33 +0100

    docs: Add Invoke tasks for running Lektor

--- tasks.py
+++ tasks.py
@@ -1,7 +1,15 @@
+from shutil import rmtree
+
 from invoke import task
 
 SRC_ENV = {'PYTHONPATH': 'src/softfab'}
 PYLINT_ENV = {'PYTHONPATH': 'tests/pylint'}
+
+@task
+def clean(c):
+    """Clean up our output."""
+    print('Cleaning up...')
+    rmtree('docs/output')
 
 @task
 def lint(c):
@@ -17,3 +25,16 @@ def run(c, host='localhost', port=8180):
             ' --listen tcp:interface=%s:port=%d'
             ' --class TwistedApp.Root' % (host, port),
             env=SRC_ENV, pty=True)
+
+@task
+def livedocs(c, host='localhost', port=5000):
+    """Serve editable version of documentation."""
+    with c.cd('docs'):
+        c.run('lektor serve --host %s --port %d' % (host, port), pty=True)
+
+@task
+def docs(c):
+    """Build documentation."""
+    with c.cd('docs'):
+        c.run('lektor build --output-path output', pty=True)
+    print('Created documentation in: docs/output')
