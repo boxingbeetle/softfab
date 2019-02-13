cee55e048ab0ccdb71dcdb3a9a9de96a0c427958
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-02-13 16:06:37 +0100

    cc: Split status models off from 'timelib' into new 'clock' module
    This simplifies dependencies.

--- src/softfab/timelib.py
+++ src/softfab/timelib.py
@@ -1,10 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-from softfab.statuslib import StatusModel, StatusModelRegistry
-from softfab.xmlgen import xml
-
-from twisted.internet import reactor
-from time import localtime, mktime, strftime, struct_time, time
+from time import localtime, mktime, struct_time, time
 from typing import Callable, Iterator, Sequence, Tuple, cast
 import re
 
@@ -57,66 +53,6 @@ timeSource = TimeSource()
 getTime = timeSource.getTime
 setTimeFunc = timeSource.setTimeFunc
 setTime = timeSource.setTime
-
-# TODO: Make separate model for second-accuracy and minute-accuracy clock.
-#       The latter is needed for updating the clock in the bottom bar if we
-#       no longer do page refreshes.
-# TODO: Use the minute-accuracy clock to trigger schedules.
-class ClockModel(StatusModel):
-    '''Model which is convenient for testing.
-
-    Command line:
-      wget --no-proxy -q -O - \
-        'http://host/path/ObserveStatus?model=clock/local&format=iso8601'
-    Substitute "-S" for "-q" to test error handling.
-    '''
-    __delayedCall  = None
-
-    @classmethod
-    def getChildClass(cls):
-        return None
-
-    def __update(self):
-        self._notify()
-        self.__delayedCall = reactor.callLater(1, self.__update)
-
-    def _registerForUpdates(self):
-        self.__delayedCall = reactor.callLater(1, self.__update)
-
-    def _unregisterForUpdates(self):
-        self.__delayedCall.cancel()
-
-    def formatIso8601(self):
-        return xml.status(
-            time = strftime('%Y-%m-%d %H:%M:%S', self.getTime())
-            )
-
-    def getTime(self):
-        return localtime()
-
-class ClockModelGroup(StatusModel):
-
-    @classmethod
-    def getChildClass(cls):
-        return ClockModel
-
-    def _createModel(self, key):
-        if key == 'local':
-            return ClockModel(key, self)
-        else:
-            raise KeyError(key)
-
-    def _iterKeys(self):
-        yield 'local'
-
-    def _registerForUpdates(self):
-        # The set of children is fixed.
-        pass
-
-    def _unregisterForUpdates(self):
-        pass
-
-StatusModelRegistry.instance.addModelGroup(ClockModelGroup, 'clock')
 
 def _intsToTime(pieces: Sequence[int]) -> int:
     return int(mktime(
