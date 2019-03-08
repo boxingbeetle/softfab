d8ee18674706f53014f1a2cad7b8bd941e88f9ff
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-03-08 13:05:17 +0100

    cc: Add type annotations to StyleResources and TwistedUtil
    There are two errors reported by mypy, but those are actual errors:
    we don't handle missing/broken style resources correctly.

--- src/softfab/StyleResources.py
+++ src/softfab/StyleResources.py
@@ -4,19 +4,22 @@ from softfab.databaselib import createInternalId
 from softfab.timelib import getTime, secondsPerDay
 from softfab.useragent import AcceptedEncodings
 from softfab.webgui import ShortcutIcon, StyleSheet, pngIcon, svgIcon
+from softfab.xmlgen import XMLNode
 from softfab import styles
 
 from importlib_resources import is_resource, read_binary
 from twisted.web.http import datetimeToString
+from twisted.web.iweb import IRequest
 from twisted.web.resource import Resource
 from twisted.web.static import Data
 
 from gzip import GzipFile
 from io import BytesIO
+from typing import Dict, Optional
 import logging
 import re
 
-def _load(fileName):
+def _load(fileName: str) -> Optional[bytes]:
     try:
         return read_binary(styles, fileName)
     except IOError as ex:
@@ -25,7 +28,11 @@ def _load(fileName):
 
 class _StyleResource(Data):
 
-    def render(self, request):
+    def __init__(self, data: bytes, mediaType: str):
+        # Implementing this to work around mypy complaining.
+        Data.__init__(self, data, mediaType)
+
+    def render(self, request: IRequest) -> bytes:
         # File expires a long time from now.
         # RFC-2616 section 14.21: "HTTP/1.1 servers SHOULD NOT send Expires
         # dates more than one year in the future."
@@ -36,7 +43,7 @@ class _StyleResource(Data):
 
 class _CompressedStyleResource(_StyleResource):
 
-    def __init__(self, data, mediaType):
+    def __init__(self, data: bytes, mediaType: str):
         _StyleResource.__init__(self, data, mediaType)
 
         # Note: Because we only compress these resources once, we might as well
@@ -48,7 +55,7 @@ class _CompressedStyleResource(_StyleResource):
 
         self.__gzippedResource = _StyleResource(gzippedData, mediaType)
 
-    def render(self, request):
+    def render(self, request: IRequest) -> bytes:
         # Pick an encoding based on client and server preferences.
         # We strongly prefer gzip because we save on bandwidth and
         # have pre-compressed the resource.
@@ -61,7 +68,7 @@ class _CompressedStyleResource(_StyleResource):
 
 _reStyleImage = re.compile(r'url\((\w+\.png)\)')
 
-def _compressableType(mediaType):
+def _compressableType(mediaType: str) -> bool:
     '''Returns True iff the given media type is suitable for compression.
     Compressing data twice only slows things down, so already compressed
     formats like PNG should not be compressed again, while text formats
@@ -78,9 +85,9 @@ class _StyleRoot(Resource):
 
     def __init__(self):
         Resource.__init__(self)
-        self.__icons = {}
+        self.__icons = {} # type: Dict[str, XMLNode]
 
-    def __addFile(self, fileName, mediaType):
+    def __addFile(self, fileName: str, mediaType: str) -> Optional[bytes]:
         data = _load(fileName)
         if data is not None:
             resourceFactory = (
@@ -91,14 +98,14 @@ class _StyleRoot(Resource):
             self.putChild(fileName.encode(), resourceFactory(data, mediaType))
         return data
 
-    def addIcon(self, name):
+    def addIcon(self, name: str) -> XMLNode:
         icon = self.__icons.get(name)
         if icon is None:
             icon = self.__addIcon(name)
             self.__icons[name] = icon
         return icon
 
-    def __addIcon(self, name):
+    def __addIcon(self, name: str) -> XMLNode:
         fileName = name + '.svg'
         if is_resource(styles, fileName):
             data = self.__addFile(fileName, 'image/svg+xml')
@@ -109,13 +116,13 @@ class _StyleRoot(Resource):
         data = self.__addFile(fileName, 'image/png')
         return pngIcon(self.relativeURL + '/' + fileName, data)
 
-    def addShortcutIcon(self, name):
+    def addShortcutIcon(self, name: str) -> ShortcutIcon:
         icon = ShortcutIcon(name, self.relativeURL)
         for fileName, mediaType in icon.iterFiles():
             self.__addFile(fileName, mediaType)
         return icon
 
-    def addStyleSheet(self, name):
+    def addStyleSheet(self, name: str) -> StyleSheet:
         fileName = name + '.css'
         data = self.__addFile(fileName, 'text/css')
         text = data.decode('utf-8')
--- src/softfab/TwistedUtil.py
+++ src/softfab/TwistedUtil.py
@@ -2,6 +2,7 @@
 
 from urllib.parse import urljoin
 
+from twisted.web.iweb import IRequest
 from twisted.web.resource import Resource
 from twisted.web.util import redirectTo
 
@@ -10,15 +11,15 @@ class PageRedirect(Resource):
     '''
     isLeaf = True
 
-    def __init__(self, page):
+    def __init__(self, page: str):
         Resource.__init__(self)
         self.page = page
 
-    def render(self, request):
+    def render(self, request: IRequest) -> bytes:
         # The Location header must have an absolute URL as its value (see
         # RFC-2616 section 14.30).
         url = urljoin(request.prePathURL(), self.page.encode())
         return redirectTo(url, request)
 
-    def getChild(self, path, request):
+    def getChild(self, path: str, request: IRequest) -> Resource:
         return self
