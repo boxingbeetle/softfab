a8c9d4d2461758b88bfadec43aaa40f11620df28
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-03-26 14:59:31 +0100

    cc: Exempt upgrade.py from import sorting
    This module is a bit peculiar and isort had made a mess of it.

--- src/softfab/upgrade.py
+++ src/softfab/upgrade.py
@@ -1,28 +1,32 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+"""
+Database upgrade script.
+
+isort:skip_file
+"""
+
 # Avoid calling fsync on record rewrites.
 # Syncing on every file would make conversions of large databases take forever.
 # The upgrade procedure states that a backup should be made before upgrading,
-import sys
-
 # so in case of abnormal termination we can just restart the upgrade from the
 # backup.
 from softfab import config
-# Set conversion flags.
-from softfab.conversionflags import (
-    setConversionFlags, setConversionFlagsForVersion
-)
-# Check whether we can convert from the database version in use before the
-# upgrade.
-from softfab.projectlib import project
-from softfab.utils import parseVersion
-
 config.dbAtomicWrites = False
 
 # pylint: disable=wrong-import-position
 
+# Set conversion flags.
+from softfab.conversionflags import (
+    setConversionFlags, setConversionFlagsForVersion
+)
 setConversionFlags()
 
+# Check whether we can convert from the database version in use before the
+# upgrade.
+from softfab.projectlib import project
+from softfab.utils import parseVersion
+import sys
 try:
     versionStr = project['version']
 except KeyError:
