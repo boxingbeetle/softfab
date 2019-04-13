ca05abacb567de1c57ae606c625e01c593802efb
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-04-13 19:33:48 +0200

    cc: Document ConnectionStatus and add NEW status
    LOST implies that the connection was operational before, so it's not
    suitable for a connection that was defined but not yet established.

--- src/softfab/connection.py
+++ src/softfab/connection.py
@@ -4,7 +4,19 @@ from enum import Enum
 
 
 class ConnectionStatus(Enum):
+    """Our perception of the status of a network connection."""
+
     UNKNOWN = 1
+    """Insufficient data on current status, was connected before."""
+
     CONNECTED = 2
+    """Recent message confirmed connection."""
+
     WARNING = 3
+    """Assumed connected, but no recent messages."""
+
     LOST = 4
+    """Currently not connected, was connected before."""
+
+    NEW = 5
+    """Was never connected."""
