29f113a64f8e1e818abed2976230bffb7b07ab9f
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-05-17 16:36:10 +0200

    cc: Add type annotations to 'shadowview' module

--- src/softfab/shadowview.py
+++ src/softfab/shadowview.py
@@ -1,32 +1,35 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
+from typing import Iterator, cast
+
 from softfab.datawidgets import (
     DataColumn, DataTable, DurationColumn, TimeColumn
 )
 from softfab.pagelinks import createTaskRunnerDetailsLink
 from softfab.shadowlib import ShadowRun, maxDoneRecords, maxOKRecords, shadowDB
 from softfab.webgui import maybeLink
-from softfab.xmlgen import xhtml
+from softfab.xmlgen import XMLContent, xhtml
 
 
-def getShadowRunStatus(run):
-    state = run['state']
-    if state == 'done':
-        return run['result'].name.lower()
+def getShadowRunStatus(run: ShadowRun) -> str:
+    if run.isDone():
+        result = run.getResult()
+        assert result is not None
+        return result.name.lower()
     else:
-        return state
+        return cast(str, run['state'])
 
 class _DescriptionColumn(DataColumn[ShadowRun]):
     keyName = 'description'
 
-    def presentCell(self, record, **kwargs):
-        return maybeLink(record.getURL())[ record['description'] ]
+    def presentCell(self, record: ShadowRun, **kwargs: object) -> XMLContent:
+        return maybeLink(record.getURL())[ record.getDescription() ]
 
 class _LocationColumn(DataColumn[ShadowRun]):
     keyName = 'location'
 
-    def presentCell(self, record, **kwargs):
-        return createTaskRunnerDetailsLink(record['location'])
+    def presentCell(self, record: ShadowRun, **kwargs: object) -> XMLContent:
+        return createTaskRunnerDetailsLink(record.getLocation())
 
 class ShadowTable(DataTable[ShadowRun]):
     widgetId = 'shadowTable'
@@ -42,7 +45,11 @@ class ShadowTable(DataTable[ShadowRun]):
         _LocationColumn.instance,
         )
 
-    def iterRowStyles(self, rowNr, record, **kwargs):
+    def iterRowStyles(self,
+                      rowNr: int,
+                      record: ShadowRun,
+                      **kwargs: object
+                      ) -> Iterator[str]:
         yield getShadowRunStatus(record)
 
 trimPolicy = xhtml.p[
