3c67423abdbbfbe84a735d060b47d7ee4531a368
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-05-31 13:02:05 +0200

    cc: Add type annotations to TaskMatrixCommon module

--- src/softfab/TaskMatrixCommon.py
+++ src/softfab/TaskMatrixCommon.py
@@ -1,26 +1,40 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from collections import defaultdict
+from typing import DefaultDict, Iterable, List, Optional
 import time
 
 from softfab.CSVPage import CSVPage
 from softfab.Page import PageProcessor
 from softfab.databaselib import RecordObserver
-from softfab.joblib import jobDB
+from softfab.joblib import Job, Task, jobDB
 from softfab.pageargs import ArgsCorrected, IntArg, PageArgs, StrArg, dynamic
 from softfab.querylib import CustomFilter, runQuery
+from softfab.request import Request
 from softfab.timelib import (
     getTime, getWeekNr, secondsPerDay, weekRange, weeksInYear
 )
+from softfab.userlib import User
 
 
-class DateRangeMonitor(RecordObserver):
-    minTime = property(lambda self: self.__minTime)
-    maxTime = property(lambda self: self.__maxTime)
-    minYear = property(lambda self: self.__minYear)
-    maxYear = property(lambda self: self.__maxYear)
+class DateRangeMonitor(RecordObserver[Job]):
+    @property
+    def minTime(self) -> int:
+        return self.__minTime
 
-    def __init__(self):
+    @property
+    def maxTime(self) -> int:
+        return self.__maxTime
+
+    @property
+    def minYear(self) -> int:
+        return self.__minYear
+
+    @property
+    def maxYear(self) -> int:
+        return self.__maxYear
+
+    def __init__(self) -> None:
         RecordObserver.__init__(self)
         # Determine minimum and maximum job time.
         createTimes = [ job.getCreateTime() for job in jobDB ]
@@ -37,42 +51,53 @@ class DateRangeMonitor(RecordObserver):
         # Register for updates.
         jobDB.addObserver(self)
 
-    def added(self, record):
+    def added(self, record: Job) -> None:
         createTime = record.getCreateTime()
         self.__maxTime = createTime
         year = time.localtime(createTime)[0]
         if year > self.__maxYear:
             self.__maxYear = year
 
-    def removed(self, record):
+    def removed(self, record: Job) -> None:
         assert False, 'job was removed'
 
-    def updated(self, record):
+    def updated(self, record: Job) -> None:
         # Create time cannot change, so we don't care.
         pass
 
 dateRange = DateRangeMonitor()
 
-def filterJobs(jobs, beginWeek, endWeek, configFilter):
+def filterJobs(jobs: Iterable[Job],
+               beginWeek: int,
+               endWeek: int,
+               configFilter: Optional[str]
+               ) -> List[Job]:
     '''Returns a new list containing those jobs from the given list
     which match the configuration and time filter.
     '''
     query = []
+
     if configFilter:
-        query.append(CustomFilter(
-            lambda job: job.getConfigId() == configFilter
-            ))
-    query.append(CustomFilter(
-        lambda job: beginWeek <= job.getCreateTime() < endWeek
-        ))
+        def configMatches(job: Job) -> bool:
+            return job.getConfigId() == configFilter
+        query.append(CustomFilter(configMatches))
+
+    def timeMatches(job: Job) -> bool:
+        return beginWeek <= job.getCreateTime() < endWeek
+    query.append(CustomFilter(timeMatches))
+
     return runQuery(query, jobs)
 
-def groupTasks(jobs, beginWeek):
+def groupTasks(jobs: Iterable[Job],
+               beginWeek: int
+               ) -> List[DefaultDict[str, List[Task]]]:
     '''Returns a list with 7 entries, one for each day of the week.
     Each entry is a dictionary with task name as key and a list of
     corresponding tasks as the value.
     '''
-    taskData = [ defaultdict(list) for _ in range(7) ]
+    taskData = [
+        defaultdict(list) for _ in range(7)
+        ] # type: List[DefaultDict[str, List[Task]]]
     # In the transition from summer time to winter time,
     # the week is 1 hour longer.
     # By assigning the 7th day dictionary to the 8th day,
@@ -98,7 +123,7 @@ class TaskMatrixCSVArgs(TaskMatrixArgs, CSVPage.Arguments):
 
 class TaskMatrixProcessor(PageProcessor[TaskMatrixCSVArgs]):
 
-    def process(self, req, user):
+    def process(self, req: Request[TaskMatrixCSVArgs], user: User) -> None:
         # TODO: It would be useful to have these as method arguments.
         year = req.args.year
         week = req.args.week
@@ -107,6 +132,9 @@ class TaskMatrixProcessor(PageProcessor[TaskMatrixCSVArgs]):
         if year is dynamic or week is dynamic:
             year = dateRange.maxYear
             week = getWeekNr(time.localtime(dateRange.maxTime))
+        else:
+            assert isinstance(year, int)
+            assert isinstance(week, int)
 
         # Bring date within a valid range.
         beginWeek, endWeek = weekRange(year, week)
