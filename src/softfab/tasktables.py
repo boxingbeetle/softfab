364ddc4976fc04baaf6033c9215e4aff50e7b302
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-06-06 15:04:57 +0200

    cc: Remove 'Data' column from tasks table
    The mid-level data can be found under its report tab. I don't think
    this will be consulted often enough to require a direct link.

--- src/softfab/tasktables.py
+++ src/softfab/tasktables.py
@@ -1,6 +1,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-from typing import Iterator, Optional, cast
+from typing import Iterator, cast
 
 from softfab.Page import InvalidRequest, PageProcessor
 from softfab.datawidgets import (
@@ -9,18 +9,15 @@ from softfab.datawidgets import (
 from softfab.joblib import Task, jobDB
 from softfab.jobview import TargetColumn
 from softfab.pagelinks import (
-    JobIdArgs, TaskIdArgs, TaskReportArgs, createTaskInfoLink,
-    createTaskRunnerDetailsLink
+    JobIdArgs, TaskIdArgs, createTaskInfoLink, createTaskRunnerDetailsLink
 )
 from softfab.projectlib import project
 from softfab.request import Request
 from softfab.resourcelib import iterTaskRunners
-from softfab.shadowlib import shadowDB
-from softfab.shadowview import getShadowRunStatus
 from softfab.taskview import getTaskStatus, taskSummary
 from softfab.typing import Collection
 from softfab.userview import OwnerColumn
-from softfab.webgui import cell, maybeLink, pageLink
+from softfab.webgui import pageLink
 from softfab.xmlgen import XMLContent, xhtml
 
 
@@ -40,50 +37,6 @@ class SummaryColumn(DataColumn[Task]):
 
     def presentCell(self, record: Task, **kwargs: object) -> XMLContent:
         return taskSummary(record)
-
-class ExtractedColumn(DataColumn[Task]):
-    label = 'Data'
-    cellStyle = 'nobreak'
-
-    def presentCell(self, record: Task, **kwargs: object) -> XMLContent:
-        proc = cast(PageProcessor, kwargs['proc'])
-        dataLink = pageLink(
-            'Task',
-            TaskReportArgs(
-                jobId=proc.args.jobId,
-                taskName=record.getName(),
-                report='data'
-                )
-            )[ 'view data' ]
-        extractionRunId = cast(Optional[str], record['extractionRun'])
-        if extractionRunId is None:
-            if record.isDone():
-                # No extraction was scheduled after task completed execution,
-                # but execution wrapper might have provided data.
-                return dataLink
-            elif record.isCancelled():
-                # Execution will not run, so no data is available.
-                return '-'
-            else:
-                # Data might become available later.
-                return 'not yet'
-        else:
-            extractionRun = shadowDB.get(extractionRunId)
-            if extractionRun is None:
-                return dataLink
-            else:
-                # Extraction run still exists.
-                logLabel, style = {
-                    'waiting': ( 'waiting', 'idle'      ),
-                    'running': ( 'running', 'busy'      ),
-                    'error':   ( 'failed',  'error'     ),
-                    'warning': ( 'warning', 'warning'   ),
-                    'ok':      ( 'log',     None        ),
-                    }[getShadowRunStatus(extractionRun)]
-                return cell(class_ = style)[
-                    dataLink, ' | ',
-                    maybeLink(extractionRun.getURL())[ logLabel ]
-                    ]
 
 class ExportColumn(DataColumn[Task]):
     label = 'Export'
@@ -211,7 +164,6 @@ class JobTaskRunsTable(TaskRunsTable):
         yield self.startTimeColumn
         yield self.durationColumn
         yield self.summaryColumn
-        yield ExtractedColumn.instance
         yield TaskRunnerColumn.instance
         # TODO: If a task does not have a report yet, hasExport() returns False.
         #       This means that if all tasks are waiting, there is no export
