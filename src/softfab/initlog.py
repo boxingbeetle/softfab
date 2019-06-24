7c14a28689d67f6a5b16a9ab0e7dd364dd0786bb
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-06-24 16:50:23 +0200

    cc: Drop _convertLoggingLevel() function
    The logging module now accepts string for the level as well.

--- src/softfab/initlog.py
+++ src/softfab/initlog.py
@@ -7,24 +7,11 @@ import os.path
 from softfab.config import dbDir, loggingLevel
 from softfab.version import VERSION
 
-
-def _convertLoggingLevel(level):
-    if isinstance(level, int):
-        return level
-    else:
-        # NOTE: getLevelName can also convert back to integer,
-        # but that feature is not documented in the Python manual
-        intLevel = logging.getLevelName(level)
-        if isinstance(intLevel, int):
-            return intLevel
-        else:
-            raise ValueError(intLevel)
-
 if not os.path.exists(dbDir):
     os.makedirs(dbDir)
 
 logging.basicConfig(
-    level = _convertLoggingLevel(loggingLevel),
+    level = loggingLevel,
     format = '* %(asctime)s %(levelname)-8s> %(message)s',
     filename = dbDir + '/cc-log.txt', filemode = 'a'
     )
