75cfc6e8ee322d7e1dc1166c9ef94ef94fa9698a
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-08-12 22:00:15 +0200

    cc: Fix link to user manual in new factories

--- src/softfab/pages/Home.py
+++ src/softfab/pages/Home.py
@@ -92,7 +92,7 @@ class Home_GET(FabPage['Home_GET.Processor', FabPage.Arguments]):
         if len(jobDB) < 20:
             yield xhtml.p[
                 'For help to get started, please read the ',
-                docLink('/reference/user_manual/')[ 'User Manual' ],
+                docLink('/start/user_manual/')[ 'User Manual' ],
                 ' or go to the general ',
                 docLink('/')[ 'Documentation page' ], '.'
                 ]
