e105cc976fb71ed2de3df15c195c90a57379a384
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-08-21 09:54:10 +0200

    cc: Install custom signal handler so "twist" doesn't re-raise SIGINT
    Without this, collected coverage data doesn't get written to file.

--- src/softfab/TwistedApp.py
+++ src/softfab/TwistedApp.py
@@ -19,6 +19,23 @@ def _createRoot(**config):
 
     return SoftFabRoot(**config)
 
+def _signalHandler(signum, frame):
+    from twisted.python import log
+    log.msg('Received SIGINT, stopping reactor.')
+    from twisted.internet import reactor
+    reactor.callFromThread(reactor.stop)
+
+def _installSignalHandler():
+    """Install our own handler for SIGINT.
+
+    If the reactor handles the signal, 'twist' will re-raise it with
+    the default signal handler set, causing the process to exit abruptly,
+    which in turn means the coverage tool can't save its collected data.
+    We avoid this by installing our own handler.
+    """
+    import signal
+    signal.signal(signal.SIGINT, _signalHandler)
+
 # The "twist" launcher will call one of these functions to create
 # the root resource.
 # Currently the root runs the startup process, so there must be exactly
@@ -38,6 +55,7 @@ def production():
 def debug():
     """Creates a root resource for development sites.
     """
+    _installSignalHandler()
     return _createRoot(
         debugSupport=True,
         anonOperator=True,
@@ -48,6 +66,7 @@ def debugAuth():
     """Creates a root resource for development sites,
     with mandatory authentication.
     """
+    _installSignalHandler()
     return _createRoot(
         debugSupport=True,
         anonOperator=False,
