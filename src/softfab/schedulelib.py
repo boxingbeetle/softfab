boxingbeetle/softfab

commit 5cab478d52a3381fb651daf9a0ed8079f1a63655

cc: Ignore case on 'done' and 'trigger' schedule properties
This fixes a bug where schedules would be reverted to not-done and
not-triggered every time the CC was restarted.

That bug was introduced a week ago in commit 088d2b33 (cc: Explicitly
support 'bool' as XML attribute value type), in which the text
representation of booleans was changed to lower case and XMLTag was
updated to match. The problem is that this update did not include
properties that weren't registered with XMLTag as boolProperties.

Since the 'done' and 'trigger' properties are each only present for
one particular sequence type, making them boolProperties is not an
option here. Instead, I fixed the custom parse code.

src/softfab/schedulelib.py
@@ -239,12 +239,13 @@ class Scheduled(XMLTag, SelectableRecordABC):
 
         sequence = self._properties['sequence']
         if sequence is ScheduleRepeat.ONCE:
-            self._properties['done'] = self._properties.get('done') == 'True'
+            self._properties['done'] = \
+                cast(str, self._properties.get('done')).lower() == 'true'
         else:
             assert 'done' not in self._properties
         if sequence is ScheduleRepeat.TRIGGERED:
             self._properties['trigger'] = (
-                self._properties.get('trigger') == 'True'
+                cast(str, self._properties.get('trigger')).lower() == 'true'
                 )
         else:
             assert 'trigger' not in self._properties

commit 7e9d0b9bfe9955d271498c2b10f76016654dc051

cc: Fix 'done' and 'trigger' schedule properties
5cab478d (cc: Ignore case on 'done' and 'trigger' schedule properties)
introduced a bug when creating new schedules: since the 'done' or
'trigger' property didn't exist yet, get() would return None, which
would then get lower() called on it.

This was hidden from mypy by the typecast. XMLTag was designed before
type annotations existed and doesn't play well with them.

src/softfab/schedulelib.py
@@ -240,12 +240,12 @@ class Scheduled(XMLTag, SelectableRecordABC):
         sequence = self._properties['sequence']
         if sequence is ScheduleRepeat.ONCE:
             self._properties['done'] = \
-                cast(str, self._properties.get('done')).lower() == 'true'
+                cast(str, self._properties.get('done', '')).lower() == 'true'
         else:
             assert 'done' not in self._properties
         if sequence is ScheduleRepeat.TRIGGERED:
             self._properties['trigger'] = (
-                cast(str, self._properties.get('trigger')).lower() == 'true'
+                cast(str, self._properties.get('trigger', '')).lower() == 'true'
                 )
         else:
             assert 'trigger' not in self._properties