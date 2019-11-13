37aa2ff6db6f802df583afb3365d02f4a420a504
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-11-13 04:17:51 +0100

    cc: Do not create directory prior to writing log file
    We write the log file in the DB directory and in the new approach we
    expect the DB directory to be created before the Control Center is
    started, since it contains the mandatory configuration file.

--- src/softfab/initlog.py
+++ src/softfab/initlog.py
@@ -1,14 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 import logging
-import os
-import os.path
 
 from softfab.config import dbDir
 from softfab.version import VERSION
-
-if not os.path.exists(dbDir):
-    os.makedirs(dbDir)
 
 logging.basicConfig(
     level = logging.INFO,
