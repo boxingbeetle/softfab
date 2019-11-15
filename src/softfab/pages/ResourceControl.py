a740d66502e42037db3df89cb22483d778028f8a
Maarten ter Huurne <maarten@boxingbeetle.com>
2019-11-15 06:29:16 +0100

    cc: Complete type annotations of ResourceControl page
    When an anonymous user suspends or resumes a resource, the string
    "anonymous" is now stored as the name of the user that made the
    last change, rather than storing None. While storing None is probably
    harmless in practice, it violates the type annotations of ResourceBase
    and may not be harmless in the future.
    
    A different solution would be to remove the 'changeduser' key from
    the ResourceBase properties if an anonymous user made the change,
    but we already have "ControlCenter" as a possible value, so having
    a visible name is more consistent.

--- src/softfab/pages/ResourceControl.py
+++ src/softfab/pages/ResourceControl.py
@@ -5,6 +5,7 @@ from enum import Enum
 from softfab.ControlPage import ControlPage
 from softfab.Page import InvalidRequest, PageProcessor
 from softfab.pageargs import EnumArg, PageArgs, SetArg
+from softfab.request import Request
 from softfab.resourcelib import resourceDB
 from softfab.response import Response
 from softfab.userlib import User, checkPrivilege
@@ -24,7 +25,10 @@ class ResourceControl_POST(ControlPage['ResourceControl_POST.Arguments',
 
     class Processor(PageProcessor['ResourceControl_POST.Arguments']):
 
-        def process(self, req, user):
+        def process(self,
+                    req: Request['ResourceControl_POST.Arguments'],
+                    user: User
+                    ) -> None:
             resNames = req.args.name
             suspend = req.args.action is Actions.SUSPEND
 
@@ -46,7 +50,7 @@ class ResourceControl_POST(ControlPage['ResourceControl_POST.Arguments',
 
             userName = user.name
             for res in resources:
-                res.setSuspend(suspend, userName)
+                res.setSuspend(suspend, userName or 'anonymous')
 
     def writeReply(self, response: Response, proc: Processor) -> None:
         response.writeXML(xml.ok)
