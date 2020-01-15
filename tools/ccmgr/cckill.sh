edd659f0cd94cc10133ea40d764352fc70c2fbb5
Hans Spanjers <hans@boxingbeetle.com>
2020-01-15 16:16:13 +0100

    ccmgr: Improvements w.r.t. VENV and CC identification
    The following improvements implemented:
    - If VENV layer is not active 'ccstart' will start it
    - A CC is identified by its name (basename dir)
    - Some minor improvements

--- tools/ccmgr/cckill.sh
+++ tools/ccmgr/cckill.sh
@@ -1,41 +1,42 @@
 #!/bin/bash
 
-DBDIR="$PWD/db"
+FACDIR="$PWD"
+FACTORY=`basename "$FACDIR"`
+DBDIR="$FACTORY/db"
 PID_FILE="$DBDIR/cc.pid"
 
 KILLED="no"
 
 if [ ! -d "$DBDIR" ]; then
-    echo "$PWD is not a CC directory"
+    echo "$FACDIR is not a CC directory"
     exit 1
 fi
 
 if [ -f "$PID_FILE" ]; then
     CC_PID=`cat "$PID_FILE"`
-    
+
     if [ -d "/proc/$CC_PID" ]; then
         # Shut down Control Center.
-        echo "Shutting down CC..."
+        echo "Shutting down CC '$FACTORY'..."
         kill -s SIGTERM "$CC_PID"
-        for count in $(seq 1 10)
-        do
+        for count in $(seq 10); do
             if [ ! -d "/proc/$CC_PID" ]; then
                 break
             fi
             sleep 1
         done
         if [ -d "/proc/$CC_PID" ]; then
-            echo "Graceful shutdown failed, now killing CC."
+            echo "Graceful shutdown failed, now killing CC '$FACTORY'."
             kill -s SIGKILL "$CC_PID"
         fi
-            KILLED="yes"
+        KILLED="yes"
     else
         echo "Cleaned stale pid file ($CC_PID)"
     fi
 fi
 
 if [ "$KILLED" == "yes" ]; then
-    echo "CC shut down ($CC_PID)"
+    echo "CC '$FACTORY' shut down ($CC_PID)"
 else
-    echo "CC was not running"
+    echo "CC '$FACTORY' was not running"
 fi
--- tools/ccmgr/ccstart.sh
+++ tools/ccmgr/ccstart.sh
@@ -1,23 +1,20 @@
 #!/bin/bash
 
-DBDIR="$PWD/db"
+FACDIR="$PWD"
+FACTORY=`basename "$FACDIR"`
+DBDIR="$FACDIR/db"
 PID_FILE="$DBDIR/cc.pid"
-LOG_FILE="$PWD/cc.log"
-
-if [ -z $VIRTUAL_ENV ]; then
-    echo "VENV is not running"
-    exit 1
-fi
+LOG_FILE="$FACDIR/$FACTORY.log"
 
 if [ ! -d "$DBDIR" ]; then
-    echo "$PWD is not a CC directory"
+    echo "$FACDIR is not a CC directory"
     exit 1
 fi
 
 if [ -f "$PID_FILE" ]; then
     CC_PID=`cat "$PID_FILE"`
     if [ -d "/proc/$PID" ]; then
-        echo "CC already running ($CC_PID)"
+        echo "CC '$FACTORY' already running ($CC_PID)"
         exit 0
     else
         echo "Cleaned stale pid file ($CC_PID)"
@@ -25,18 +22,25 @@ if [ -f "$PID_FILE" ]; then
     fi
 fi
 
+if [ -z $VIRTUAL_ENV ]; then
+    if [ ! -f "$FACDIR/venv/bin/activate" ]; then
+        echo "Not possible to create VENV"
+        exit 1
+    fi
+    source "$FACDIR"/venv/bin/activate
+fi
+
 softfab server --dir "$DBDIR" 2> "$LOG_FILE" &
 
 if [ $? -eq 0 ]; then
-    for count in $(seq 1 10)
-    do
+    for count in $(seq 10); do
         if [ -f "$PID_FILE" ]; then
             break
         fi
         sleep 1
     done
     CC_PID=`cat "$PID_FILE"`
-    echo "CC started ($CC_PID)"
+    echo "CC '$FACTORY' started ($CC_PID)"
 else
-    echo "Could not launch CC"
+    echo "Could not launch CC '$FACTORY'"
 fi
