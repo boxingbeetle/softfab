b557a396b03358a08725b926f36d4f3efbc35144
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-02-11 04:59:56 +0100

    cc: Remove obsolete 'dev.sh' development helper script
    This isn't necessary anymore now that "poetry env use" lets us pick
    a Python version.

--- dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Start Poetry with Python 3.x as the default Python.
-
-cd `dirname $0`
-
-test -d tools/venv3 || virtualenv -p python3 tools/venv3
-
-test -d tools/venv3 && PATH="$PWD/tools/venv3/bin:$PATH" exec poetry shell
