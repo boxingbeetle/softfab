2bb0d887b810f09e2632f1a6f18712dcf079246c
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-03-03 06:04:12 +0100

    cc: Store graph builders on Execution Graph doc page in tuple iso dict
    The dictionary was never used for lookups, only iterated through in
    its entirity.

--- src/softfab/docs/concepts/exegraph/__init__.py
+++ src/softfab/docs/concepts/exegraph/__init__.py
@@ -16,56 +16,54 @@ button = 'Graph'
 children = ()
 icon = 'IconDesign'
 
-graphBuilders = {
-    builder.name: builder
-    for builder in (
-        ExecutionGraphBuilder(
-            'task',
-            links=False,
-            frameworks=(
-                Framework.create('build', (), ()),
-                ),
+graphBuilders = (
+    ExecutionGraphBuilder(
+        'task',
+        links=False,
+        frameworks=(
+            Framework.create('build', (), ()),
             ),
-        ExecutionGraphBuilder(
-            'product',
-            links=False,
-            products=(
-                ProductDef.create('binary'),
-                ),
+        ),
+    ExecutionGraphBuilder(
+        'product',
+        links=False,
+        products=(
+            ProductDef.create('binary'),
             ),
-        ExecutionGraphBuilder(
-            'dependency',
-            links=False,
-            products=(
-                ProductDef.create('binary'),
-                ),
-            frameworks=(
-                Framework.create('build', (), ('binary',)),
-                Framework.create('test', ('binary',), ())
-                ),
+        ),
+    ExecutionGraphBuilder(
+        'dependency',
+        links=False,
+        products=(
+            ProductDef.create('binary'),
             ),
-        ExecutionGraphBuilder(
-            'token',
-            links=False,
-            products=(
-                ProductDef.create('app_installed', prodType=ProductType.TOKEN),
-                ),
+        frameworks=(
+            Framework.create('build', (), ('binary',)),
+            Framework.create('test', ('binary',), ())
             ),
-        ExecutionGraphBuilder(
-            'combined',
-            links=False,
-            products=(
-                ProductDef.create('coverage_data', combined=True),
-                ),
+        ),
+    ExecutionGraphBuilder(
+        'token',
+        links=False,
+        products=(
+            ProductDef.create('app_installed', prodType=ProductType.TOKEN),
             ),
-        )
-    }
+        ),
+    ExecutionGraphBuilder(
+        'combined',
+        links=False,
+        products=(
+            ProductDef.create('coverage_data', combined=True),
+            ),
+        ),
+    )
 
 graphRenders: Dict[str, XMLContent] = {}
 
 def process() -> Optional[Deferred]:
     deferreds = []
-    for name, builder in graphBuilders.items():
+    for builder in graphBuilders:
+        name = builder.name
         if name not in graphRenders:
             d = builder.build(export=False).toSVG()
             d.addCallback(partial(graphRenders.__setitem__, name))
