boxingbeetle/softfab

commit 6532c8817803941f872a1c9bfa8db0a460b96df6

cc: Merge instead of overwrite 'class' attribute on hgroup/vgroup

src/softfab/webgui.py
@@ -19,7 +19,7 @@ import logging
 from softfab.compat import NoReturn
 from softfab.pageargs import PageArgs, Query
 from softfab.pnglib import getPNGDimensions
-from softfab.utils import SharedInstance, iterable
+from softfab.utils import SharedInstance, abstract, iterable
 from softfab.xmlgen import (
     XML, XMLContent, XMLNode, XMLPresentable, XMLSubscriptable, adaptToXML,
     xhtml
@@ -174,6 +174,23 @@ class AttrContainer(Container):
                 attributes[key] = value
         return self._replaceAttributes(attributes)
 
+    # TODO: This is a modified copy-paste of XMLNode.addClass().
+    #       It would be better to have a class for attributes and
+    #       implement this method there.
+    def addClass(self: AttrContainerT, name: Optional[str]) -> AttrContainerT:
+        '''Appends the given `name` to our `class` attribute.
+        This can be used to add a CSS class without overwriting an existing one.
+        Returns a new container, or this one if `name` was None.
+        '''
+        if name is None:
+            return self
+
+        attrs = dict(self._attributes)
+        oldClass = attrs.get('class')
+        newClass = name if oldClass is None else f'{oldClass} {name}'
+        attrs['class'] = newClass
+        return self._replaceAttributes(attrs)
+
 class _GroupItem(AttrContainer):
     '''Wraps an item that is contained within a group container.
     '''
@@ -203,6 +220,7 @@ class _Group(AttrContainer):
     '''
     groupTag = xhtml.div
     itemTag = xhtml.div
+    cssClass: str = abstract
 
     def _adaptContentElement(self, element: XMLContent) -> Iterator[XML]:
         yield adaptToXML(cast(XMLPresentable, groupItem.adapt(element)))
@@ -215,17 +233,25 @@ class _Group(AttrContainer):
             if presentation
             ]
         if presentations:
-            return self.groupTag(**self._attributes)[ presentations ]
+            return self.groupTag(**self._attributes).addClass(self.cssClass)[
+                presentations
+                ]
         else:
             return None
 
-hgroup = _Group((), dict(class_ = 'hgroup'))
+class _HGroup(_Group):
+    cssClass = 'hgroup'
+
+class _VGroup(_Group):
+    cssClass = 'vgroup'
+
+hgroup = _HGroup((), {})
 '''Container that groups page elements horizontally, aligned to top.
 Items with an empty presentation will not have cells allocated to them.
 If all items present empty, the group itself is omitted.
 '''
 
-vgroup = _Group((), dict(class_ = 'vgroup'))
+vgroup = _VGroup((), {})
 '''Container that groups page elements vertically, with equal width.
 Items with an empty presentation will not have cells allocated to them.
 If all items present empty, the group itself is omitted.

commit 9028bacff7a37738494d2dcbf58c0131801a2497

cc: Fix presentation of unorderedList widget
This broke in 6532c881 (cc: Merge instead of overwrite 'class'
attribute on hgroup/vgroup), because I overlooked the fact that
_Group is a base class for not just hgroup/vgroup but also for
unorderedList.

APE didn't catch the problem in CI because we currently only run APE
on a Control Center with an empty database; we need to also run it
on a populated Control Center.

One of our PyLint plugins did catch the problem, but because we have
so many unresolved errors reported by PyLint, that went unnoticed.

src/softfab/webgui.py
@@ -260,6 +260,7 @@ If all items present empty, the group itself is omitted.
 class _UnorderedList(_Group):
     groupTag = xhtml.ul
     itemTag = xhtml.li
+    cssClass = None
 
 unorderedList = _UnorderedList((), {})
 '''Container that displays its items as an unordered (bullet) list.