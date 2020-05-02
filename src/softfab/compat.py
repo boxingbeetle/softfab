fb5abe3a707f7d7f9b68d5d497affc26cb1cab38
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-05-02 03:09:46 +0200

    cc: Take version from package metadata
    This avoids having the version in multiple places. It also means that
    snapshot wheels should now include the exact git hash they were built
    from.
    
    Originally I didn't want to take this step yet, see removed TODO
    comment. But I think that for the near future, it's fine to require
    installations to be done via pip.

--- src/softfab/compat.py
+++ src/softfab/compat.py
@@ -14,6 +14,13 @@ if TYPE_CHECKING:
 else:
     NoReturn = None
 
+# On Python 3.8+, use importlib.metadata from the standard library.
+# On older versions, a compatibility package can be installed from PyPI.
+try:
+    if not TYPE_CHECKING:
+        import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
 
 # On Python 3.7+, use importlib.resources from the standard library.
 # On older versions, a compatibility package can be installed from PyPI.
@@ -24,4 +31,6 @@ except ImportError:
     import importlib_resources
 
 
-__all__ = ['NoReturn', 'Protocol', 'importlib_resources']
+__all__ = [
+    'NoReturn', 'Protocol', 'importlib_metadata', 'importlib_resources'
+    ]
--- src/softfab/version.py
+++ src/softfab/version.py
@@ -1,13 +1,9 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
-VERSION = '3.0.0-pre1'
+from softfab.compat import importlib_metadata
+
+VERSION = importlib_metadata.version('softfab')
 """SoftFab version."""
 
 HOMEPAGE = 'https://boxingbeetle.com/tools/softfab/'
 """The SoftFab homepage."""
-
-# TODO: In the future we should read these from the package metadata using
-#       importlib.metadata (Python 3.8) or importlib_metadata (compat package).
-#       However, Debian's python3-importlib-metadata isn't packaged for Buster
-#       at the moment (not in backports either), so it's probably too early
-#       to require it as a dependency.
