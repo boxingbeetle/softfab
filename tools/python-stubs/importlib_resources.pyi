7d2b5ab03b2648838407938f51d1a8f56d69d677
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-05-02 03:00:45 +0200

    cc: Add type stubs for importlib.metadata and resources
    These stubs are shipped as part of mypy's included copy of typeshed,
    but they have a version check to prevent the interface from being
    available on Python versions that don't have these libraries.
    
    I copied the stubs from typeshed, added a header to document their
    origin, disabled the version check and renamed them to function as
    type stubs for the importlib_metadata and importlib_resources
    backports.
    
    The end result is that we can type check code using these modules
    on any Python version we support.

---
+++ tools/python-stubs/importlib_resources.pyi
@@ -0,0 +1,30 @@
+# SPDX-License-Identifier: Apache-2.0
+#
+# Source: https://github.com/python/typeshed/
+# Modifications: Disabled the version check.
+
+import sys
+# This is a >=3.7 module, so we conditionally include its source.
+if True: #sys.version_info >= (3, 7):
+    import os
+
+    from pathlib import Path
+    from types import ModuleType
+    from typing import ContextManager, Iterator, Union, BinaryIO, TextIO
+
+    Package = Union[str, ModuleType]
+    Resource = Union[str, os.PathLike]
+
+    def open_binary(package: Package, resource: Resource) -> BinaryIO: ...
+    def open_text(package: Package,
+                  resource: Resource,
+                  encoding: str = ...,
+                  errors: str = ...) -> TextIO: ...
+    def read_binary(package: Package, resource: Resource) -> bytes: ...
+    def read_text(package: Package,
+                  resource: Resource,
+                  encoding: str = ...,
+                  errors: str = ...) -> str: ...
+    def path(package: Package, resource: Resource) -> ContextManager[Path]: ...
+    def is_resource(package: Package, name: str) -> bool: ...
+    def contents(package: Package) -> Iterator[str]: ...
