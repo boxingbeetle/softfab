417d5ea388cf750a468088a927457cc08fbbdab6
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-05-11 22:16:57 +0200

    cc: Add jsonToData() function
    This performs the reverse operation of dataToJSON(): creating a data
    transfer object from a JSON object.

--- src/softfab/json.py
+++ src/softfab/json.py
@@ -5,7 +5,7 @@ Data transfer objects are defined using the `attrs` library.
 """
 
 from enum import Enum
-from typing import Dict
+from typing import Dict, Type, TypeVar
 
 import attr
 
@@ -27,3 +27,73 @@ def dataToJSON(data: object) -> Dict[str, object]:
         else:
             raise TypeError(value.__class__.__name__)
     return jsonNode
+
+def _describeType(typ: type) -> str:
+    """Describe the given Python type in a way that makes sense for
+    a user providing data in JSON format.
+    """
+    if issubclass(typ, str):
+        return 'string'
+    elif issubclass(typ, bool):
+        return 'Boolean'
+    elif issubclass(typ, int):
+        return 'integer'
+    elif issubclass(typ, float):
+        return 'floating point'
+    elif hasattr(typ, 'items'):
+        return 'object'
+    elif hasattr(typ, '__iter__'):
+        return 'array'
+    else:
+        return typ.__name__
+
+T = TypeVar('T')
+
+def jsonToData(jsonNode: object, cls: Type[T]) -> T:
+    """Create a data transfer object from a JSON dictionary.
+    Raise ValueError if the structure of the JSON data does not map
+    to the given data transfer class.
+    """
+
+    try:
+        iterItems = getattr(jsonNode, 'items')
+    except AttributeError:
+        raise ValueError(f"Expected object, "
+                         f"got {_describeType(jsonNode.__class__)}'")
+
+    fields = attr.fields_dict(cls)
+    kwargs: Dict[str, object] = {}
+    for name, value in iterItems():
+        try:
+            attrib = fields.pop(name)
+        except KeyError:
+            raise ValueError(f"Field '{name}' does not exist")
+
+        attribType = attrib.type
+        if attribType is None:
+            # We raise TypeError since this is an error on the data
+            # transfer class instead of invalid JSON data.
+            raise TypeError(f"No type specified for attribute '{name}'")
+
+        if issubclass(attribType, Enum):
+            if not isinstance(value, str):
+                raise ValueError(f"Expected string value "
+                                 f"for field '{name}'")
+            memberName = value.upper()
+            try:
+                kwargs[name] = attribType.__members__[memberName]
+            except KeyError:
+                raise ValueError(
+                    f"Invalid value '{value}' for field '{name}'; "
+                    f"expected one of: " + ', '.join(
+                        mbr.lower() for mbr in attribType.__members__))
+        else:
+            if not isinstance(value, attribType):
+                raise ValueError(f"Expected {_describeType(attribType)} "
+                                 f"value for field '{name}'")
+            kwargs[name] = value
+
+    if fields:
+        raise ValueError(f"Missing values for fields: {', '.join(fields)}")
+
+    return cls(**kwargs) # type: ignore[call-arg]
--- tests/unit/test_json.py
+++ tests/unit/test_json.py
@@ -3,8 +3,9 @@
 from enum import Enum, auto
 
 import attr
+from pytest import raises
 
-from softfab.json import dataToJSON
+from softfab.json import dataToJSON, jsonToData
 
 
 class Color(Enum):
@@ -26,3 +27,36 @@ knightJSON = dict(name='Launcelot', age=35, knighted=True, fav_color='blue')
 def testHeroDataToJSON():
     """Test whether a we can create JSON from a simple object."""
     assert dataToJSON(knight) == knightJSON
+
+def testJSONToHeroData():
+    """Test whether a we can create a simple object from JSON."""
+    assert jsonToData(knightJSON, HeroData) == knight
+
+def testBadJSON():
+    """Test the various ways in which JSON binding can fail."""
+
+    with raises(ValueError, match="Expected object, got array"):
+        jsonToData([], HeroData)
+
+    with raises(ValueError, match="Missing values for fields:"):
+        jsonToData({}, HeroData)
+
+    with raises(ValueError, match="Field 'quest' does not exist"):
+        jsonToData({'quest': 'Holy Grail'}, HeroData)
+
+    with raises(ValueError, match="Expected integer value for field 'age'"):
+        jsonToData(dict(knightJSON, age='unknown'), HeroData)
+
+    with raises(ValueError, match="Expected string value for field 'fav_color'"):
+        jsonToData(dict(knightJSON, fav_color=None), HeroData)
+
+    with raises(ValueError, match="Invalid value 'yellow' for field 'fav_color'; "
+                                  "expected one of: red, green, blue"):
+        jsonToData(dict(knightJSON, fav_color='yellow'), HeroData)
+
+def testBadDataClass():
+    """Test handling of errors in the passed data class."""
+
+    BrokenData = attr.make_class('BrokenData', ['emblem'])
+    with raises(TypeError, match="No type specified for attribute 'emblem'"):
+        jsonToData({'emblem': 'chicken'}, BrokenData)
