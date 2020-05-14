boxingbeetle/softfab

commit 7aaef23d7148c861ef3132187b145975c14ac739

cc: Always call __init__() using super()
We used a mixture of explicit __init__() calls and super() before,
but that can lead to double calls.

src/softfab/resourcelib.py
@@ -25,7 +25,7 @@ from softfab.xmlgen import XMLContent, xml
 
 ResourceT = TypeVar('ResourceT', bound='ResourceBase')
 
-class ResourceBase(ParamMixin, XMLTag, DatabaseElem):
+class ResourceBase(XMLTag, ParamMixin, DatabaseElem):
     """Base class for Resource and TaskRunner.
     """
     tagName: ClassVar[str] = abstract
@@ -33,13 +33,11 @@ class ResourceBase(ParamMixin, XMLTag, DatabaseElem):
     intProperties = ('changedtime',)
 
     def __init__(self, properties: Mapping[str, str]):
-        ParamMixin.__init__(self)
         if 'locator' in properties:
             # COMPAT 2.x.x: Make locator into a parameter.
             properties = dict(properties)
             self.addParameter('locator', properties.pop('locator'))
-        XMLTag.__init__(self, properties)
-        DatabaseElem.__init__(self)
+        super().__init__(properties)
         self._capabilities: AbstractSet[str] = set()
         self.__token: Optional[Token] = None
 
@@ -253,7 +251,7 @@ class TaskRunnerData(XMLTag):
     tagName = 'data'
 
     def __init__(self, properties: Mapping[str, str]):
-        XMLTag.__init__(self, properties)
+        super().__init__(properties)
         self._properties.setdefault('host', '?')
         self.__run = cast(RunInfo, None)
 
@@ -319,7 +317,7 @@ class ExecutionObserver(RecordObserver[TaskRun]):
                  taskRunner: 'TaskRunner',
                  callback: Callable[[Optional[TaskRun]], None]
                  ):
-        RecordObserver.__init__(self)
+        super().__init__()
         self.__taskRunner = taskRunner
         self.__callback: Callable[[Optional[TaskRun]], None] = callback
         self._run: Optional[TaskRun] = None
@@ -441,7 +439,7 @@ class TaskRunner(ResourceBase):
             properties = dict(properties, suspended=properties['paused'])
             del properties['paused']
 
-        ResourceBase.__init__(self, properties)
+        super().__init__(properties)
         self._properties.setdefault('description', '')
         self._properties.setdefault('status', ConnectionStatus.NEW)
         self.__data: Optional[TaskRunnerData] = None

src/softfab/EditPage.py
@@ -49,6 +49,7 @@ class AbstractPhase(Generic[EditProcT, EditArgsT, DBRecord]):
     '''
 
     def __init__(self, page: 'EditPage[EditArgsT, DBRecord]'):
+        super().__init__()
         self.page = page
 
     def process(self, proc: EditProcT) -> None:
@@ -436,7 +437,7 @@ class EditPage(FabPage[EditProcessorBase[EditArgsT, DBRecord], EditArgsT], ABC):
         raise NotImplementedError
 
     def __init__(self) -> None:
-        FabPage.__init__(self)
+        super().__init__()
         self.editPhase = EditPhase[EditArgsT, DBRecord](self)
         self.savePhase = SavePhase[EditArgsT, DBRecord](self)
         self.saveAsPhase = SaveAsPhase[EditArgsT, DBRecord](self)

src/softfab/FabPage.py
@@ -299,7 +299,7 @@ class FabPage(BasePage[ProcT, ArgsT]):
 class _WidgetResponder(Responder):
 
     def __init__(self, page: BasePage, widget: Widget, proc: PageProcessor):
-        Responder.__init__(self)
+        super().__init__()
         self.__page = page
         self.__widget = widget
         self.__proc = proc
@@ -326,6 +326,7 @@ class LinkBarButton:
                  modifier: IconModifier = IconModifier.NONE,
                  active: bool = False
                  ):
+        super().__init__()
         self.label = label
         self.url = url
         self.icon = icon

src/softfab/Page.py
@@ -82,7 +82,7 @@ class Redirect(BaseException):
     page.
     '''
     def __init__(self, url: str):
-        BaseException.__init__(self)
+        super().__init__()
         self.url = url
 
 class PageProcessor(Generic[ArgsT]):
@@ -104,6 +104,7 @@ class PageProcessor(Generic[ArgsT]):
                  args: ArgsT,
                  user: User
                  ):
+        super().__init__()
         self.page = page
         self.req = req
         self.args = args
@@ -177,7 +178,7 @@ class Responder:
 class Redirector(Responder):
 
     def __init__(self, url: str):
-        Responder.__init__(self)
+        super().__init__()
         self.__url = url
 
     def respond(self, response: Response) -> None:
@@ -186,7 +187,7 @@ class Redirector(Responder):
 class HTTPAuthenticator(Responder):
 
     def __init__(self, realm: str, message: Optional[str] = None):
-        Responder.__init__(self)
+        super().__init__()
         self.__realm = realm
         self.__message = message
 

src/softfab/RecordDelete.py
@@ -21,7 +21,7 @@ class RecordInUseError(Exception):
                  refererName: str,
                  presenter: Callable[[str], XMLContent],
                  referers: Collection[str]):
-        Exception.__init__(self, 'record in use')
+        super().__init__('record in use')
         self.refererName = refererName
         self.presenter = presenter
         self.referers = referers

src/softfab/SplashPage.py
@@ -14,6 +14,7 @@ from softfab.xmlgen import XMLContent, xhtml
 class _StartupMessages:
 
     def __init__(self) -> None:
+        super().__init__()
         self.__messages = [ 'Server startup initiated' ]
 
     def __iter__(self) -> Iterator[str]:

src/softfab/StyleResources.py
@@ -30,10 +30,6 @@ def _load(fileName: str) -> Optional[bytes]:
 
 class _StyleResource(Data):
 
-    def __init__(self, data: bytes, mediaType: str):
-        # Implementing this to work around mypy complaining.
-        Data.__init__(self, data, mediaType)
-
     def render(self, request: IRequest) -> bytes:
         # File expires a long time from now.
         # RFC-2616 section 14.21: "HTTP/1.1 servers SHOULD NOT send Expires
@@ -46,7 +42,7 @@ class _StyleResource(Data):
 class _CompressedStyleResource(_StyleResource):
 
     def __init__(self, data: bytes, mediaType: str):
-        _StyleResource.__init__(self, data, mediaType)
+        super().__init__(data, mediaType)
 
         # Note: Because we only compress these resources once, we might as well
         #       do it with maximum compression.
@@ -82,7 +78,7 @@ class _StyleRoot(Resource):
     relativeURL = 'styles'
 
     def __init__(self) -> None:
-        Resource.__init__(self)
+        super().__init__()
         self.__icons: Dict[str, Image] = {}
 
     def __addFile(self, fileName: str, mediaType: str) -> Optional[bytes]:

src/softfab/TwistedRoot.py
@@ -40,6 +40,7 @@ class ChunkCaller:
             gen: Iterator[Union[Generator, Callable[[], None], None]],
             deferred: Deferred
             ):
+        super().__init__()
         self.__gen = gen
         self.__deferred = deferred
         self.scheduleNext()
@@ -97,6 +98,7 @@ class DatabaseLoader:
     recordChunks = 100
 
     def __init__(self, root: 'SoftFabRoot'):
+        super().__init__()
         self.root = root
 
     def process(self) -> Iterator[None]:
@@ -111,6 +113,7 @@ class DatabaseLoader:
 class PageLoader:
 
     def __init__(self, root: 'SoftFabRoot'):
+        super().__init__()
         self.root = root
 
     def __addPage(self, module: ModuleType, pageName: str) -> None:
@@ -271,7 +274,7 @@ class SoftFabRoot(Resource):
             #       overhauled before we'll even consider multiple roots.
             DocPage.authenticator = DisabledAuthPage.instance
 
-        Resource.__init__(self)
+        super().__init__()
         self.putChild(b'', PageRedirect('Home'))
         self.putChild(styleRoot.relativeURL.encode(), styleRoot)
 

commit efb411a6432f11f5679102025c93df9564adf9af

cc: Fix migration of 'locator' attribute of resources
This fixed field was migrated to a parameter, but the parameter was
set before the super __init__ methods were run, which meant the
dictionary to store the parameters in wasn't initialized yet.

This got broken in 7aaef23d (Always call __init__() using super()).

src/softfab/resourcelib.py
@@ -36,8 +36,12 @@ class ResourceBase(XMLTag, ParamMixin, DatabaseElem):
         if 'locator' in properties:
             # COMPAT 2.x.x: Make locator into a parameter.
             properties = dict(properties)
-            self.addParameter('locator', properties.pop('locator'))
+            locator = properties.pop('locator')
+        else:
+            locator = None
         super().__init__(properties)
+        if locator is not None:
+            self.addParameter('locator', locator)
         self._capabilities: AbstractSet[str] = set()
         self.__token: Optional[Token] = None
 