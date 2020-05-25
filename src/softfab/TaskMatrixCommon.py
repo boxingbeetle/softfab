boxingbeetle/softfab

commit c0e956005673353ff67bbebe89a122d202da9fab

cc: Move dateRange from TaskMatrixCommon to joblib module
Although the task matrix is currently the only place this is used,
the date range for the job DB is a property of the job DB, so having
this functionality in joblib makes more sense.

src/softfab/TaskMatrixCommon.py
@@ -1,71 +1,18 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from collections import defaultdict
+from time import localtime
 from typing import ClassVar, DefaultDict, Iterable, List, Optional
-import time
 
 from softfab.CSVPage import CSVPage
 from softfab.Page import PageProcessor
-from softfab.databaselib import RecordObserver
-from softfab.joblib import Job, JobDB, Task, jobDB
+from softfab.joblib import Job, JobDB, Task, dateRange
 from softfab.pageargs import ArgsCorrected, IntArg, PageArgs, StrArg, dynamic
 from softfab.querylib import CustomFilter, runQuery
 from softfab.request import Request
-from softfab.timelib import (
-    getTime, getWeekNr, secondsPerDay, weekRange, weeksInYear
-)
+from softfab.timelib import getWeekNr, secondsPerDay, weekRange, weeksInYear
 from softfab.userlib import User
 
-
-class DateRangeMonitor(RecordObserver[Job]):
-    @property
-    def minTime(self) -> int:
-        return self.__minTime
-
-    @property
-    def maxTime(self) -> int:
-        return self.__maxTime
-
-    @property
-    def minYear(self) -> int:
-        return self.__minYear
-
-    @property
-    def maxYear(self) -> int:
-        return self.__maxYear
-
-    def __init__(self) -> None:
-        super().__init__()
-        # Determine minimum and maximum job time.
-        createTimes = [ job.getCreateTime() for job in jobDB ]
-        if createTimes:
-            self.__minTime = min(createTimes)
-            self.__maxTime = max(createTimes)
-        else:
-            now = getTime()
-            self.__minTime = now
-            self.__maxTime = now
-        self.__minYear = time.localtime(self.__minTime)[0]
-        self.__maxYear = time.localtime(self.__maxTime)[0]
-
-        # Register for updates.
-        jobDB.addObserver(self)
-
-    def added(self, record: Job) -> None:
-        createTime = record.getCreateTime()
-        self.__maxTime = createTime
-        year = time.localtime(createTime)[0]
-        if year > self.__maxYear:
-            self.__maxYear = year
-
-    def removed(self, record: Job) -> None:
-        assert False, 'job was removed'
-
-    def updated(self, record: Job) -> None:
-        # Create time cannot change, so we don't care.
-        pass
-
-dateRange = DateRangeMonitor()
 
 def filterJobs(jobs: Iterable[Job],
                beginWeek: int,
@@ -133,7 +80,7 @@ class TaskMatrixProcessor(PageProcessor[TaskMatrixArgs]):
         # Use week of last report as default.
         if year is dynamic or week is dynamic:
             year = dateRange.maxYear
-            week = getWeekNr(time.localtime(dateRange.maxTime))
+            week = getWeekNr(localtime(dateRange.maxTime))
         else:
             assert isinstance(year, int)
             assert isinstance(week, int)
@@ -142,10 +89,10 @@ class TaskMatrixProcessor(PageProcessor[TaskMatrixArgs]):
         beginWeek, endWeek = weekRange(year, week)
         if beginWeek < dateRange.minTime:
             year = dateRange.minYear
-            week = getWeekNr(time.localtime(dateRange.minTime))
+            week = getWeekNr(localtime(dateRange.minTime))
         elif beginWeek > dateRange.maxTime:
             year = dateRange.maxYear
-            week = getWeekNr(time.localtime(dateRange.maxTime))
+            week = getWeekNr(localtime(dateRange.maxTime))
         week = max(1, min(week, weeksInYear(year)))
 
         if year != req.args.year or week != req.args.week:

src/softfab/joblib.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict
 from itertools import chain
+from time import localtime
 from typing import (
     TYPE_CHECKING, AbstractSet, Callable, DefaultDict, Dict, Iterable,
     Iterator, List, Mapping, MutableSet, Optional, Sequence, Tuple, Union, cast
@@ -1126,3 +1127,53 @@ def iterUnfinishedTasks(taskFilter: Iterable[str]) -> Iterator[Task]:
         for task in iterAllTasks(taskFilter)
         if not task.hasResult()
         )
+
+class DateRangeMonitor(RecordObserver[Job]):
+    @property
+    def minTime(self) -> int:
+        return self.__minTime
+
+    @property
+    def maxTime(self) -> int:
+        return self.__maxTime
+
+    @property
+    def minYear(self) -> int:
+        return self.__minYear
+
+    @property
+    def maxYear(self) -> int:
+        return self.__maxYear
+
+    def __init__(self) -> None:
+        super().__init__()
+        # Determine minimum and maximum job time.
+        createTimes = [ job.getCreateTime() for job in jobDB ]
+        if createTimes:
+            self.__minTime = min(createTimes)
+            self.__maxTime = max(createTimes)
+        else:
+            now = getTime()
+            self.__minTime = now
+            self.__maxTime = now
+        self.__minYear = localtime(self.__minTime)[0]
+        self.__maxYear = localtime(self.__maxTime)[0]
+
+        # Register for updates.
+        jobDB.addObserver(self)
+
+    def added(self, record: Job) -> None:
+        createTime = record.getCreateTime()
+        self.__maxTime = createTime
+        year = localtime(createTime)[0]
+        if year > self.__maxYear:
+            self.__maxYear = year
+
+    def removed(self, record: Job) -> None:
+        assert False, 'job was removed'
+
+    def updated(self, record: Job) -> None:
+        # Create time cannot change, so we don't care.
+        pass
+
+dateRange = DateRangeMonitor()

commit 6744747a5b4809e0a9bdb1d4cabc53ccd313ace3

cc: Inject dateRange as a dependency
This makes the initialization order explicit: the DateRangeMonitor
is only created after the jobs have been loaded.

Fixes an issue introduced in c0e95600 (cc: Move dateRange from
TaskMatrixCommon to joblib module) where existing jobs were not
included in the date range because the DateRangeMonitor was
constructed before jobDB was preloaded.

src/softfab/TaskMatrixCommon.py
@@ -6,7 +6,7 @@ from typing import ClassVar, DefaultDict, Iterable, List, Optional
 
 from softfab.CSVPage import CSVPage
 from softfab.Page import PageProcessor
-from softfab.joblib import Job, JobDB, Task, dateRange
+from softfab.joblib import DateRangeMonitor, Job, JobDB, Task
 from softfab.pageargs import ArgsCorrected, IntArg, PageArgs, StrArg, dynamic
 from softfab.querylib import CustomFilter, runQuery
 from softfab.request import Request
@@ -71,11 +71,13 @@ class TaskMatrixCSVArgs(TaskMatrixArgs, CSVPage.Arguments):
 class TaskMatrixProcessor(PageProcessor[TaskMatrixArgs]):
 
     jobDB: ClassVar[JobDB]
+    dateRange: ClassVar[DateRangeMonitor]
 
     async def process(self, req: Request[TaskMatrixArgs], user: User) -> None:
         # TODO: It would be useful to have these as method arguments.
         year = req.args.year
         week = req.args.week
+        dateRange = self.dateRange
 
         # Use week of last report as default.
         if year is dynamic or week is dynamic:

src/softfab/TwistedRoot.py
@@ -28,6 +28,7 @@ from softfab.config import dbDir
 from softfab.databaselib import Database
 from softfab.databases import iterDatabases
 from softfab.docserve import DocPage, DocResource
+from softfab.joblib import DateRangeMonitor, JobDB
 from softfab.pageargs import PageArgs
 from softfab.render import NotFoundPage, renderAuthenticated
 from softfab.schedulelib import ScheduleManager
@@ -317,7 +318,12 @@ class SoftFabRoot(Resource):
     def startup(self) -> Generator:
         databaseLoader = DatabaseLoader()
         yield databaseLoader.process()
-        yield PageLoader(self, databaseLoader.databases).process
+        databases = databaseLoader.databases
+        dependencies: Dict[str, object] = dict(
+            databases,
+            dateRange=DateRangeMonitor(cast(JobDB, databases['jobDB']))
+            )
+        yield PageLoader(self, dependencies).process
         # Start schedule processing.
         yield ScheduleManager().trigger
 

src/softfab/joblib.py
@@ -1148,7 +1148,7 @@ class DateRangeMonitor(RecordObserver[Job]):
     def maxYear(self) -> int:
         return self.__maxYear
 
-    def __init__(self) -> None:
+    def __init__(self, jobDB: JobDB) -> None:
         super().__init__()
         # Determine minimum and maximum job time.
         createTimes = [ job.getCreateTime() for job in jobDB ]
@@ -1178,5 +1178,3 @@ class DateRangeMonitor(RecordObserver[Job]):
     def updated(self, record: Job) -> None:
         # Create time cannot change, so we don't care.
         pass
-
-dateRange = DateRangeMonitor()

src/softfab/pages/TaskMatrix.py
@@ -9,7 +9,7 @@ import time
 from softfab.CSVPage import presentCSVLink
 from softfab.FabPage import FabPage
 from softfab.TaskMatrixCommon import (
-    TaskMatrixArgs, TaskMatrixCSVArgs, TaskMatrixProcessor, dateRange
+    TaskMatrixArgs, TaskMatrixCSVArgs, TaskMatrixProcessor
 )
 from softfab.configlib import ConfigDB
 from softfab.formlib import dropDownList, emptyOption, makeForm, submitButton
@@ -38,6 +38,7 @@ class NavigationBar(Table):
         assert isinstance(week, int)
         year = args.year
         assert isinstance(year, int)
+        dateRange = proc.dateRange
         yield (
             cell[
                 'Year: ',