73d650bc87ee5c48b9d31375a8fa66cbf10281d2
Maarten ter Huurne <maarten@boxingbeetle.com>
2020-08-10 22:33:06 +0200

    cc: Convert test_resourcelib to pytest style

--- tests/unit/test_resourcelib.py
+++ tests/unit/test_resourcelib.py
@@ -4,72 +4,62 @@
 test_resource_requirements suite.
 """
 
-import unittest
+# Import for the side effect of setting dbDir.
+# We're not going to use that dir, but resourcelib still needs it.
+import initconfig
 
-from initconfig import removeDB
-
-from softfab import databases, resourcelib
+from softfab.resourcelib import ResourceDB
 
 
-def resourcesOfType(typeName):
-    return sorted(resourcelib.resourceDB.resourcesOfType(typeName))
+def testResourcesOfType(tmp_path):
+    """Test ResourceDB.resourcesOfType() method."""
 
-class TestResources(unittest.TestCase):
-    """Test resources."""
+    def createDB():
+        resourceDB = ResourceDB(tmp_path)
+        resourceDB.factory.resTypeDB = None
+        resourceDB.preload()
+        return resourceDB
 
-    def __init__(self, methodName = 'runTest'):
-        unittest.TestCase.__init__(self, methodName)
+    def resourcesOfType(typeName):
+        return sorted(resourceDB.resourcesOfType(typeName))
 
-    def reloadDatabases(self):
-        databases.reloadDatabases()
+    resourceDB = createDB()
 
-    def setUp(self):
-        self.reloadDatabases()
+    res1 = resourceDB.factory.newResource('R1', 'TA', '', ())
+    res2 = resourceDB.factory.newResource('R2', 'TA', '', ())
+    res3 = resourceDB.factory.newResource('R3', 'TB', '', ())
+    res4 = resourceDB.factory.newResource('R4', 'TB', '', ())
 
-    def tearDown(self):
-        removeDB()
+    # DB is empty.
+    assert resourcesOfType('TA') == []
+    assert resourcesOfType('TB') == []
 
-    def test0100ResourcesOfType(self):
-        """Test resourceDB.resourcesOfType() method."""
+    # Add records.
+    resourceDB.add(res1)
+    assert resourcesOfType('TA') == ['R1']
+    assert resourcesOfType('TB') == []
+    resourceDB.add(res4)
+    assert resourcesOfType('TA') == ['R1']
+    assert resourcesOfType('TB') == ['R4']
+    resourceDB.add(res3)
+    assert resourcesOfType('TA') == ['R1']
+    assert resourcesOfType('TB') == ['R3', 'R4']
+    resourceDB.add(res2)
+    assert resourcesOfType('TA') == ['R1', 'R2']
+    assert resourcesOfType('TB') == ['R3', 'R4']
 
-        resourceFactory = resourcelib.resourceDB.factory
-        res1 = resourceFactory.newResource('R1', 'TA', '', ())
-        res2 = resourceFactory.newResource('R2', 'TA', '', ())
-        res3 = resourceFactory.newResource('R3', 'TB', '', ())
-        res4 = resourceFactory.newResource('R4', 'TB', '', ())
+    # Remove record.
+    resourceDB.remove(res4)
+    assert resourcesOfType('TA') == ['R1', 'R2']
+    assert resourcesOfType('TB') == ['R3']
 
-        # DB is empty.
-        self.assertEqual(resourcesOfType('TA'), [])
-        self.assertEqual(resourcesOfType('TB'), [])
+    # Change type of record.
+    res1Bv2 = resourceDB.factory.newResource('R2', 'TB', '', ())
+    resourceDB.update(res1Bv2)
+    assert resourcesOfType('TA') == ['R1']
+    assert resourcesOfType('TB') == ['R2', 'R3']
 
-        # Add records.
-        resourcelib.resourceDB.add(res1)
-        self.assertEqual(resourcesOfType('TA'), ['R1'])
-        self.assertEqual(resourcesOfType('TB'), [])
-        resourcelib.resourceDB.add(res4)
-        self.assertEqual(resourcesOfType('TA'), ['R1'])
-        self.assertEqual(resourcesOfType('TB'), ['R4'])
-        resourcelib.resourceDB.add(res3)
-        self.assertEqual(resourcesOfType('TA'), ['R1'])
-        self.assertEqual(resourcesOfType('TB'), ['R3', 'R4'])
-        resourcelib.resourceDB.add(res2)
-        self.assertEqual(resourcesOfType('TA'), ['R1', 'R2'])
-        self.assertEqual(resourcesOfType('TB'), ['R3', 'R4'])
-
-        # Remove record.
-        resourcelib.resourceDB.remove(res4)
-        self.assertEqual(resourcesOfType('TA'), ['R1', 'R2'])
-        self.assertEqual(resourcesOfType('TB'), ['R3'])
-
-        # Change type of record.
-        res1Bv2 = resourceFactory.newResource('R2', 'TB', '', ())
-        resourcelib.resourceDB.update(res1Bv2)
-        self.assertEqual(resourcesOfType('TA'), ['R1'])
-        self.assertEqual(resourcesOfType('TB'), ['R2', 'R3'])
-
-        self.reloadDatabases()
-        self.assertEqual(resourcesOfType('TA'), ['R1'])
-        self.assertEqual(resourcesOfType('TB'), ['R2', 'R3'])
-
-if __name__ == '__main__':
-    unittest.main()
+    # Reload DB.
+    resourceDB = createDB()
+    assert resourcesOfType('TA') == ['R1']
+    assert resourcesOfType('TB') == ['R2', 'R3']
